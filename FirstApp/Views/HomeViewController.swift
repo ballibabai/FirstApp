b36e818b44ffecd9feb37dd80a02db9c5da010e4
İbrahim Ballıbaba <ibo@192.168.1.84>
2024-12-15 01:29:08 +0300

    first commit

---
+++ FirstApp/Views/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  FirstApp
+//
+//  Created by İbrahim Ballıbaba on 15.12.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemCyan
+    }
+}
